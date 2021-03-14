289ffb4d1ae09d85556af72e548498d70a64baf1
Alberto Arias <alberg.arias88@gmail.com>
2021-03-14 11:45:40 +0100

    Subo los ejercicios de la practica resueltos y un gitignore en el general

---
+++ practica1/EJerciciosPractica/Exercises/Badsort_ptr/badsort-ptr.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+typedef struct {
+    char data[4096];
+    int key;
+} item;
+
+item array[] = {
+    {"bill", 3},
+    {"neil", 4},
+    {"john", 2},
+    {"rick", 5},
+    {"alex", 1},
+};
+
+void sort(item *a, int n) {
+    int i = 0, j = 0;
+    int s = 1;
+    item* p;
+
+    for(; (i < n) && (s != 0); i++) {
+        s = 0;
+        p = a;
+        j = n-2;
+        do {
+            if( (p+s)->key > (p+s+1)->key) {
+                item t = *(p+s);
+                *(p+s)  = *(p+s+1);
+                *(p+s+1) = t;                
+            }
+            s++;  
+        } while ( --j >= 0 );
+       
+    }
+}
+
+int main() {
+    int i;
+    sort(array,5);
+    for(i = 0; i < 5; i++)
+        printf("array[%d] = {%s, %d}\n",
+                i, array[i].data, array[i].key);
+    return 0;
+}
